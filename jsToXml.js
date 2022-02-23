1b818e655b116d255b4da4656717c87fd8de0b12
Karina Moura <57039290+kfmr@users.noreply.github.com>
2022-02-23 18:43:13 -0300

    refatorada estrutura de pastas

--- jsToXml.js
+++
@@ -1,7 +0,0 @@
-const jsontoxml = require('jsontoxml')
-module.exports = jsToXml((data, tagSingular, tagPlural) => {
-    let tag = tagSingular
-    return jsontoxml({
-        [tag]: data
-    })
-})
